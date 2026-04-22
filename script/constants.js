099c4699831fcb7ba3d5851a7f39a83f0061c8be
Roman Vitanza <rvitanza@cap.gov>
2026-04-22 12:17:36 -0500

    Refactor constants and add default state values

--- script/constants.js
+++ script/constants.js
@@ -1,12 +1,22 @@
 // constants.js
 
-export const LOGO_URL = "./LogoNoAux.png";
+// --- Image Assets ---
+export const LOGO_URL = "./LogoNoAux.png"; // located at repo root
 
+// --- Signature Formats ---
+export const SIGNATURE_FORMATS = {
+  GENERIC: "generic",
+  PLAINTEXT: "plaintext",
+  MOBILE: "mobile"
+};
+
+// --- Member Types ---
 export const MEMBER_TYPES = {
   SENIOR: "senior",
   CADET: "cadet"
 };
 
+// --- Grade Types ---
 export const GRADE_TYPES = {
   OFFICER: "officer",
   NCO: "nco",
@@ -14,18 +24,21 @@ export const GRADE_TYPES = {
   CADET_NCO: "cadet-nco"
 };
 
-export const SIGNATURE_FORMATS = {
-  GENERIC: "generic",
-  PLAINTEXT: "plaintext",
-  MOBILE: "mobile"
-};
-
+// --- Default State ---
 export const DEFAULT_STATE = {
   type: MEMBER_TYPES.SENIOR,
   gradeType: GRADE_TYPES.OFFICER,
-  vals: {}
+  vals: {
+    name: "",
+    grade: "",
+    duty: "",
+    unit: "",
+    email: "",
+    phone: ""
+  }
 };
 
+// --- Field Names (for form inputs) ---
 export const FIELD_NAMES = {
   NAME: "name",
   GRADE: "grade",
