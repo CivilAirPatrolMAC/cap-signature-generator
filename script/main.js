bf5fc41a1edbbcccc43a411be0766f7a4e1f7e6c
Roman Vitanza <rvitanza@cap.gov>
2026-04-22 12:03:00 -0500

    Initialize app on DOMContentLoaded event

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
+import { initUI } from "./ui.js";
+import { initializeState } from "./state.js";
+import { renderPreview } from "./preview.js";
 
+function initApp() {
+  initializeState();
+  initUI();
+  renderPreview();
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
